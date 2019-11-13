4275f05b0febb9d64671244d53aa056a94986d74
Timofei1 <57501337+Timofei1@users.noreply.github.com>
2019-11-13 18:20:40 +0400

    Updates

---
+++ precache-manifest.fd615d73694ae18283fb311da4be4643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d35f922e47b169734415291ddc6a84b",
+    "url": "/vk-apps/index.html"
+  },
+  {
+    "revision": "f39cb3ca59790c3d2b87",
+    "url": "/vk-apps/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "a28a167c9ebeec5e8a2d",
+    "url": "/vk-apps/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f39cb3ca59790c3d2b87",
+    "url": "/vk-apps/static/js/2.933a63b1.chunk.js"
+  },
+  {
+    "revision": "a28a167c9ebeec5e8a2d",
+    "url": "/vk-apps/static/js/main.2b8d79ed.chunk.js"
+  },
+  {
+    "revision": "5a3c23a951ef3c03ca61",
+    "url": "/vk-apps/static/js/runtime-main.ea4dd233.js"
+  },
+  {
+    "revision": "3ec91a8a32d3dc8b483c927fc9fb634d",
+    "url": "/vk-apps/static/media/persik.3ec91a8a.png"
+  }
+]);
