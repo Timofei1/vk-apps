92aa5d9e261b070d141c941eb05b9f8809525c21
Timofei1 <57501337+Timofei1@users.noreply.github.com>
2019-11-13 19:21:54 +0400

    vk-apps

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -42,27 +42,7 @@ Home.propTypes = {
 			title: PropTypes.string,
 		}),
 	}),
-	<Group title="Navigation Example">
-			<Div>
-				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Кликни, там паук валера
-				</Button>
-			</Div>
-		</Group>
-	</Panel>
-);
-
-Home.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
-};
+}
 
 export default Home;
+
